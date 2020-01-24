2251d11375fe7468fbf65471643892b3de35ea7c
smfasihaly <smfasihali@gmail.com>
2020-01-25 03:43:32 +0500

    POST method included

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import json
 app = Flask(__name__)
 
 class Account:
-    def __init__(self, Name, Phone, Website):
-        self.Name = Name
-        self.Phone = Phone
-        self.Website = Website          
-@app.route('/')
+    Name = ""
+    Phone = ""
+    Website = "" 
+    def __init__(self, Name= None, Phone=None, Website=None,jsonObject = None):
+        if(jsonObject== None):
+            self.Name = Name
+            self.Phone = Phone
+            self.Website = Website
+        else:
+            self.__dict__ = json.loads(json.dumps(jsonObject))          
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    lst = []
-    for i in range(5):
-        acc =  Account("rizwan new "+str(i),"0333","www,fasih.com")
-        lst.append(acc)
-    print(lst, lambda x: x.__dict__ )
-    return json.dumps(lst,default = lambda x: x.__dict__)
-
+    if request.method == "POST":
+        _json = Account(request.json)
+        return _json.Name
+    else:
+        lst = []
+        for i in range(5):
+            acc =  Account("rizwan new "+str(i),"0333","www,fasih.com")
+            lst.append(acc)
+        print(lst, lambda x: x.__dict__ )
+        return json.dumps(lst,default = lambda x: x.__dict__)
+@app.route('/test',methods = ["POST"])
+def addStudent():
+    '''
+    if you want to deserialize complete object you must write jsonObject in paremeter
+    '''
+    acc =Account(jsonObject =request.json)
+    return jsonify({"isSuccess":"true"})
 if __name__ == '__main__':
     app.run(debug=True)
